5e604e14be6057c453f7254312b6a09419311477
Chris Tankersley <chris@ctankersley.com>
2022-05-24 10:11:50 -0400

    Few fixes for signalling and pointing to prod OT JS

--- src/Action/SignalAction.php
+++ src/Action/SignalAction.php
@@ -2,9 +2,9 @@
 
 namespace OTHelloWorld\Action;
 
+use Laminas\Diactoros\Response\EmptyResponse;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
-use Laminas\Diactoros\Response\JsonResponse;
 use Psr\Http\Message\ServerRequestInterface;
 use Vonage\Client;
 
@@ -29,5 +29,7 @@ class SignalAction
         $sessionId = $data['sessionId'];
         $signalData = 'Signal from server at ' . date('l jS \of F Y h:i:s A');
         $this->vonage->video()->sendSignal($sessionId, 'from-server', $signalData);
+
+        return new EmptyResponse();
     }
 }
